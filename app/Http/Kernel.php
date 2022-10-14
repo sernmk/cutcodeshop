8c62e95cf8db35536c50a25101831a5b8012530f
sernmk <sernmk@gmail.com>
2022-10-14 22:00:29 +0300

    Stage 1 Done!

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,6 +30,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            //TODO добавить рейтлимит global
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
