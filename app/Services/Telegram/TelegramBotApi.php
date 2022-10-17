080cf33c7497b528e54d37a2103d9cb8c4b3f00d
sernmk <sernmk@gmail.com>
2022-10-17 22:44:55 +0300

    stage2 - intermediate

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -10,11 +10,17 @@ final class TelegramBotApi
 {
     public const HOST = 'https://api.telegram.org/bot';
 
-    public static function sendMessage(string $token, int $chatId, string $text): void
+    public static function sendMessage(string $token, int $chatId, string $text): bool
     {
-        Http::get(self::HOST . $token . '/sendMessage', [
-            'chat_id' => $chatId,
-            'text' => $text
-        ]);
+        try {
+            Http::get(self::HOST . $token . '/sendMessage', [
+                'chat_id' => $chatId,
+                'text' => $text
+            ]);
+        } catch (\Exception $exception) {
+            return false;
+        }
+        return true;
+
     }
 }
