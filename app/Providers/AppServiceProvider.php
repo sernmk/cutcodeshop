fd79aca93da27ee31e9d53e65f2b5d65e6ecd52d
sernmk <sernmk@gmail.com>
2022-10-22 07:13:08 +0300

    refactoring

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,22 +11,8 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
 //        Model::preventLazyLoading(!app()->isProduction());
 //        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
@@ -34,22 +20,16 @@ class AppServiceProvider extends ServiceProvider
         Model::shouldBeStrict(!app()->isProduction());
 
         if (app()->isProduction()) {
-            DB::whenQueryingForLongerThan(CarbonInterval::seconds(5), function (Connection $connection) {
-                logger()
-                    ->channel('telegram')
-                    ->debug('whenQueryingForLongerThan: ' . $connection->totalQueryDuration());
-            });
 
             DB::listen(function ($query) {
                 if ($query->time > 100) {
                     logger()
                         ->channel('telegram')
-                        ->debug('whenQueryingForLongerThan: ' . $query->sql, $query->bindings);
+                        ->debug('query longer than 1ms: ' . $query->sql, $query->bindings);
                 }
             });
 
-            $kernel = app(Kernel::class);
-            $kernel->whenRequestLifecycleIsLongerThan(
+            app(Kernel::class)->whenRequestLifecycleIsLongerThan(
                 CarbonInterval::seconds(4),
                 function () {
                     logger()
--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -4,7 +4,9 @@ declare(strict_types = 1);
 
 namespace App\Services\Telegram;
 
+use App\Services\Telegram\Exceptions\TelegramBotApiException;
 use Illuminate\Support\Facades\Http;
+use Throwable;
 
 final class TelegramBotApi
 {
@@ -13,14 +15,16 @@ final class TelegramBotApi
     public static function sendMessage(string $token, int $chatId, string $text): bool
     {
         try {
-            Http::get(self::HOST . $token . '/sendMessage', [
+            $response = Http::get(self::HOST . $token . '/sendMessage', [
                 'chat_id' => $chatId,
                 'text' => $text
-            ]);
-        } catch (\Exception $exception) {
+            ])->throw()->json();
+
+            return $response['ok'] ?? false;
+        } catch (Throwable $exception) {
+            report(new TelegramBotApiException($exception->getMessage()));
+
             return false;
         }
-        return true;
-
     }
 }
