196f942125666afb39471c890d9a95312dcd4301
sernmk <sernmk@gmail.com>
2022-11-19 13:42:02 +0300

    refactoring

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,16 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Domain\Catalog\Models\Brand;
 use Domain\Catalog\Models\Category;
+use Domain\Catalog\ViewModels\CategoryViewModel;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        $categories = Category::query()->homePage()->get();
+        $categories = CategoryViewModel::make()
+            ->homePage();
 
-        $products = Product::query()->homePage()->get();
+        $products = Product::query()
+            ->homePage()
+            ->get();
 
-        $brands = Brand::query()->homePage()->get();
+        $brands = Brand::query()
+            ->homePage()
+            ->get();
 
         return view('index', compact('categories', 'products', 'brands'));
     }
--- src/Domain/Catalog/Models/Brand.php
+++ src/Domain/Catalog/Models/Brand.php
@@ -3,13 +3,16 @@
 namespace Domain\Catalog\Models;
 
 use App\Models\Product;
-use Illuminate\Contracts\Database\Eloquent\Builder;
+use Domain\Catalog\QueryBuilders\BrandQueryBuilder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Support\Traits\Models\HasSlug;
 use Support\Traits\Models\HasThumbnail;
 
+/**
+ * @method static Brand|BrandQueryBuilder query()
+ */
 class Brand extends Model
 {
     use HasFactory;
@@ -29,11 +32,9 @@ class Brand extends Model
         return 'brands';
     }
 
-    public function scopeHomePage(Builder $query)
+    public function newEloquentBuilder($query): BrandQueryBuilder
     {
-        $query->where('on_home_page', true)
-            ->orderBy('sorting')
-            ->limit(6);
+        return new BrandQueryBuilder($query);
     }
 
     public function products(): HasMany
